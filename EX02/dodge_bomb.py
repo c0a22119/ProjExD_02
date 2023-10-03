1d5f1db7fff77c33b8041605555baeeecd389f58
hanamori <c0a221193f@edu.teu.ac.jp>
2023-10-03 14:47:57 +0900

    ren5

--- EX02/dodge_bomb.py
+++ EX02/dodge_bomb.py
@@ -5,8 +5,6 @@ import random
 WIDTH, HEIGHT = 1600, 900
 BOMB_RADIUS = 10
 BOMB_COLOR = (255, 0, 0)  # 赤色
-vx = 5
-vy = 5
 KEY_MOVEMENTS = {
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, 5),
@@ -28,13 +26,22 @@ def main():
     tmr = 0
 
     # 爆弾Surfaceの作成
-    bomb_surface = pg.Surface((BOMB_RADIUS*2, BOMB_RADIUS*2))
-    bomb_surface.fill((0, 0, 0))
-    pg.draw.circle(bomb_surface, BOMB_COLOR, (BOMB_RADIUS, BOMB_RADIUS), BOMB_RADIUS)
-    bomb_surface.set_colorkey((0, 0, 0))
+    bomb_surface = pg.Surface((20, 20))
+    bomb_surface.fill((0, 0, 0))  # 黒で塗りつぶす
+    pg.draw.circle(bomb_surface, BOMB_COLOR, (10, 10), 10)  # 赤い円を描画
+    bomb_surface.set_colorkey((0, 0, 0))  # 黒を透明にする
+
+    # 爆弾Rectのランダムな位置を設定
     bomb_rect = bomb_surface.get_rect()
-    bomb_rect.x = random.randint(0, WIDTH - BOMB_RADIUS*2)
-    bomb_rect.y = random.randint(0, HEIGHT - BOMB_RADIUS*2)
+    bomb_rect.topleft = (random.randint(0, WIDTH - 20), random.randint(0, HEIGHT - 20))
+
+    # 爆弾の移動方向を設定（初期値はTrue）
+    bomb_dir_x = True
+    bomb_dir_y = True
+
+    # 爆弾の速度を設定
+    vx = 5
+    vy = 5
 
     while True:
         for event in pg.event.get():
@@ -43,10 +50,10 @@ def main():
 
         key_lst = pg.key.get_pressed()
         dx, dy = 0, 0
-        for key, (vx, vy) in KEY_MOVEMENTS.items():
+        for key, (vx_key, vy_key) in KEY_MOVEMENTS.items():
             if key_lst[key]:
-                dx += vx
-                dy += vy
+                dx += vx_key
+                dy += vy_key
         kk_rect.move_ip(dx, dy)
         inside_x, inside_y = is_inside_screen(kk_rect)
         if not inside_x:
@@ -58,11 +65,13 @@ def main():
         bomb_rect.move_ip(vx, vy)
         inside_x, inside_y = is_inside_screen(bomb_rect)
         if not inside_x:
-            vx = -vx
-            bomb_rect.move_ip(2*vx, 0) 
+            vx = -vx  # 速度の符号を反転
         if not inside_y:
-            vy = -vy
-            bomb_rect.move_ip(0, 2*vy)
+            vy = -vy  # 速度の符号を反転
+
+        # こうかとんと爆弾が衝突したかどうかを判定
+        if kk_rect.colliderect(bomb_rect):
+            return  # 衝突した場合、main関数からreturnする
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rect.topleft)
